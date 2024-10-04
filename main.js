17fbf6f0bb2f303f1c0c10239f2b7af174ffeac4
Juan Almodóvar <almodovar.juan7@gmail.com>
2024-10-04 19:27:34 +0200

    initial commit (2.")

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+import * as THREE from 'three';
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({canvas});
+
+    // More ThreeJS code goes here...
+}
+
+main();
